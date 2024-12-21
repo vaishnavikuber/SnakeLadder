e898649ad783fabdc83b4723625bbbbaf319ae46
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-21 10:52:41 +0530

    roll a die

---
+++ SnakeLadder/RollDie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadder
+{
+    internal class RollDie
+    {
+        public static void Roll()
+        {
+            Random random = new Random();
+            int Die = random.Next(1, 6);
+            Console.WriteLine($"Your Die number : {Die}");
+        }
+
+    }
+}
