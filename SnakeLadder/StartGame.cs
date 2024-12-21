07913ef7e235e3d49681b257ec854a90e3758e12
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-21 10:32:25 +0530

    start position

---
+++ SnakeLadder/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadder
+{
+    internal class StartGame
+    {
+
+
+        public static void Start()
+        {
+            int start = 0;
+            Console.WriteLine($" Your start position is {start}" );
+        }
+
+
+    }
+}
