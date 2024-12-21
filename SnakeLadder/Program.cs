e8a74e516e62955153de00aa1687b3421abe213b
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-21 10:53:23 +0530

    added roll a die

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -6,7 +6,8 @@ namespace SnakeLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StartGame.Start();
+            RollDie.Roll();
         }
     }
 }
