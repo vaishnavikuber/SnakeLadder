810bacd86161a9bf773fc09914c37017a1b83d75
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-21 11:30:50 +0530

    no play ladder and snake

--- SnakeLadder/OptionCheck.cs
+++ SnakeLadder/OptionCheck.cs
@@ -11,18 +11,22 @@ namespace SnakeLadder
         {
             
             Random random = new Random();
+            
             int Die = random.Next(1, 6);
             if (Die == 6 || Die == 4)
             {
-                Console.WriteLine($"You got {Die} : No Play");
+                int position= random.Next(1,100);
+                Console.WriteLine($"You got {Die} : No Play . You will stay in {position}");
             }
             else if(Die == 2 || Die == 3)
             {
-                Console.WriteLine($"Wow!! you got {Die} : ladder");
+                int position = random.Next(1, 100);
+                Console.WriteLine($"Wow!! you got {Die} : ladder . You will jump to {position+Die}");
             }
             else
             {
-                Console.WriteLine($" Bad Luck !!! you got {Die} : Snake");
+                int position = random.Next(1, 100);
+                Console.WriteLine($" Bad Luck !!! you got {Die} : Snake . You will go back to {position-Die}");
             }
         }
 
